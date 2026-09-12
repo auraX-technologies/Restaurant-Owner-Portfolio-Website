5510c893b5355e93ab02b2473f8d3e226eb22c76
auraX-technologies <auraX-technologies@users.noreply.github.com>
2026-09-12 21:27:16 +0530

    Fix react-router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <div className="flex flex-col min-h-screen bg-soft-cream">
         <Navbar />
         <main className="flex-grow">
